369fabfa67df790635a8acb93c9ec19bb7c74dc1
Jeramy Rutley <jrutley@gmail.com>
2017-03-16 14:36:26 -0400

    Added more tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -15,4 +15,22 @@ describe('number entry', () => {
         state = app.handleNumberLogic(state, '5');
         expect(state.currentValue).toEqual(55);
     })
+    it.skip('ignores decimal at end', () => {
+        let state = app.handleNumberLogic(app.state, '5');
+        state = app.handleNumberLogic(state, '.');
+        expect(state.currentValue).toEqual(5);
+    })
+    it.skip('ignores any second decimal point', () => {
+        let state = app.handleNumberLogic(app.state, '5');
+        state = app.handleNumberLogic(state, '.');
+        state = app.handleNumberLogic(app.state, '1');
+        state = app.handleNumberLogic(state, '.');
+        state = app.handleNumberLogic(state, '2');
+        expect(state.currentValue).toEqual(5.12);
+    })
+    it.skip('adds a zero at the front if decimal is first', () => {
+        let state = app.handleNumberLogic(state, '.');
+        state = app.handleNumberLogic(app.state, '1');
+        expect(state.currentValue).toEqual(0.1);
+    })
 });
